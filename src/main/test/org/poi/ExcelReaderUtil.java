2a370b2fef6ede8954db85e988c19450c3411fe6
GodYeung <2506597416@qq.com>
2018-01-20 17:38:44 +0800

    添加excel2003和excel2007两个文件的对应读取方法的整合在一个类里，方便调用

--- src/main/test/org/poi/ExcelReaderUtil.java
+++ src/main/test/org/poi/ExcelReaderUtil.java
@@ -14,8 +14,8 @@ public class ExcelReaderUtil {
 	public static final String EXCEL07_EXTENSION = ".xlsx";
 
 	/**
-	 * 每获取一条记录，即打印
-	 * 在flume里每获取一条记录即发送，而不必缓存起来，可以大大减少内存的消耗，这里主要是针对flume读取大数据量excel来说的
+	 * 每获取一条记录，即发送，而不必缓存起来，可以大大减少内存的消耗，这里主要是针对flume读取大数据量excel来说的
+	 *
 	 * @param sheetName
 	 * @param sheetIndex
 	 * @param curRow
@@ -57,7 +57,17 @@ public class ExcelReaderUtil {
 	}
 
 	public static void main(String[] args) throws Exception {
-		String path="C:\\Users\\y****\\Desktop\\TestSample\\H_20171226_***_*****_0430.xlsx";
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\Base_Date1\\H_20180111_Base_Date(4)_0420.xlsx";*/
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\REWORK\\H_20180105_Cto_REWORK_1600.xlsx";*/
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\ASS_ITEM\\H_20180116_ASS_ITEM_1915.xlsx";*/
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\Cto_Process\\H_20180116_Cto_Process_2000.xlsx";*/
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\Cto_Ship\\H_20180105_Cto_Ship_0005.xlsx";*/
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\Po_sn\\H_20180105_Po_sn_1020.xlsx";*/
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\Cto_CODE\\H_20180109_Cto_CODE_1640.xlsx";*/
+		String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\ASS_PRODUCT\\H_20171226_ASS_PRODUCT_0430 - 副本.xlsx";
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\MAC\\H_20180108_MAC_2130.xlsx";*/
+		/*String path = "C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\Base_Data3\\H_20180106_Base_Data3_1520 - 副本.xlsx";*/
+		/*String path="C:\\Users\\y15079\\Desktop\\shenjiangnan\\TestSample\\H_20171226_ASS_PRODUCT_0430.xls";*/
 		ExcelReaderUtil.readExcel(path);
 	}
 }
